50d10c66994d00d0ef2d6ad52af597f0acdc4eb5
Yuwen Zhang <yuwen.x.zhang@gmail.com>
2026-03-23 18:52:25 -0700

    Added highlighting for winning squares and message for draws.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useState } from 'react'
 
-export function Square({ value, handleClick }) {
+export function Square({ value, handleClick, isWinning}) {
   return (
     <button
-      className="square"
+      className={`square ${isWinning ? 'win-square' : ''}`}
       onClick={handleClick}
     >
       {value}
@@ -15,9 +15,12 @@ export function Square({ value, handleClick }) {
 
 export function Board({squares, xIsNext, onPlay}) {
   let status;
-  const winner = calculateWinner(squares);
+  const [winner, winningSquares] = calculateWinner(squares);
   if(winner) {
     status = "The winner is: " + winner; // winner determined
+  }
+  else if (boardFull(squares)) {
+    status = "It's a draw."
   }
   else {
     status = "Next player: " + (xIsNext ? 'X': 'O'); // next turn
@@ -40,23 +43,30 @@ export function Board({squares, xIsNext, onPlay}) {
     onPlay(nextSquares);
   }
 
+  const isWinningSquare = (i) => {
+    if (winningSquares) {
+      return winningSquares.includes(i);
+    }
+    return false;
+  }
+
   return (
     <> 
       <h1>{status}</h1>
       <div className="board-row">
-        <Square value={squares[0]} handleClick={() => markSquare(0)}/>
-        <Square value={squares[1]} handleClick={() => markSquare(1)}/>
-        <Square value={squares[2]} handleClick={() => markSquare(2)}/>
+        <Square value={squares[0]} handleClick={() => markSquare(0)} isWinning={isWinningSquare(0)}/>
+        <Square value={squares[1]} handleClick={() => markSquare(1)} isWinning={isWinningSquare(1)}/>
+        <Square value={squares[2]} handleClick={() => markSquare(2)} isWinning={isWinningSquare(2)}/>
       </div>
       <div className="board-row">
-        <Square value={squares[3]} handleClick={() => markSquare(3)}/>
-        <Square value={squares[4]} handleClick={() => markSquare(4)}/>
-        <Square value={squares[5]} handleClick={() => markSquare(5)}/>
+        <Square value={squares[3]} handleClick={() => markSquare(3)} isWinning={isWinningSquare(3)}/>
+        <Square value={squares[4]} handleClick={() => markSquare(4)} isWinning={isWinningSquare(4)}/>
+        <Square value={squares[5]} handleClick={() => markSquare(5)} isWinning={isWinningSquare(5)}/>
       </div>
       <div className="board-row">
-        <Square value={squares[6]} handleClick={() => markSquare(6)}/>
-        <Square value={squares[7]} handleClick={() => markSquare(7)}/>
-        <Square value={squares[8]} handleClick={() => markSquare(8)}/>
+        <Square value={squares[6]} handleClick={() => markSquare(6)} isWinning={isWinningSquare(6)}/>
+        <Square value={squares[7]} handleClick={() => markSquare(7)} isWinning={isWinningSquare(7)}/>
+        <Square value={squares[8]} handleClick={() => markSquare(8)} isWinning={isWinningSquare(8)}/>
       </div>
     </>
   );
@@ -78,10 +88,21 @@ function calculateWinner(squares) {
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
+      return [squares[a], lines[i]];
     }
   }
-  return null;
+  return [null, null];
+}
+
+const boardFull = (squares) => {
+  let isFull = true;
+  squares.forEach((value, i) => {
+    if(!value) {
+      console.log("value at " + i + "is empty")
+      isFull = false;
+    }
+  })
+  return isFull;
 }
 
 export default function Game() {
@@ -106,9 +127,9 @@ export default function Game() {
       display = "Go to start";
     }
     return (
-      <li key={move}>
-        <button onClick={() => jumpTo(move)}>{display}</button>
-      </li>
+      <div key={move}>
+        <button className="goto-button" onClick={() => jumpTo(move)}>{display}</button>
+      </div>
     )
   });
 
@@ -118,13 +139,16 @@ export default function Game() {
   }
 
   return (
-    <div className="game">
-      <div className="game-board">
-        <Board squares={currentSquares} xIsNext={xIsNext} onPlay={onPlay} />
+    <>
+      <div className="game">
+        <div className="game-board">
+          <Board squares={currentSquares} xIsNext={xIsNext} onPlay={onPlay} />
+        </div>
+        <div className="game-info">
+          <p>Previous moves:</p>
+          <ol>{historyDisplay}</ol>
+        </div>
       </div>
-      <div className="game-info">
-        <ol>{historyDisplay}</ol>
-      </div>
-    </div>
+    </>
   )
 }
